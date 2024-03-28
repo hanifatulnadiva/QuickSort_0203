c03d0e0990e5038a4650cef1b319fb9509188f97
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-03-28 14:15:25 +0700

    menambahkan perulangan for untuk menyimpan data pada array

--- QuickSort_0203/QuickSort_0203.cpp
+++ QuickSort_0203/QuickSort_0203.cpp
@@ -17,7 +17,16 @@ void input() {
             break;
         else
             cout << "\nMaksimum panjang array adalah 20" << endl;
-     
+  
+    }
+    cout << "\n------------------" << endl;
+    cout << "\nEnter Array Elemen" << endl;
+    cout << "\n------------------" << endl;
+
+    for (int i = 0; i < 1; i++)
+    {
+        cout << "<" << (i + 1) << "<";
+        cin >> arr[i];
     }
 }
 int main()

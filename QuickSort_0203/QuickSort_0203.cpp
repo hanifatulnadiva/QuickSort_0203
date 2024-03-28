5d61510493838c27c1ebe6085a08a592a4fbc89b
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-03-28 14:07:23 +0700

    menambahkan perulangan while pada prosedur input

--- QuickSort_0203/QuickSort_0203.cpp
+++ QuickSort_0203/QuickSort_0203.cpp
@@ -9,7 +9,17 @@ int cmp_cout = 0;
 int mov_cout = 0;
 int n;
 
-
+void input() {
+    while (true) {
+        cout << "masukan jumlah data pada array:"; 
+        cin >> n; 
+        if (n <= 20) 
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+        
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
